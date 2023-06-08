7aace6ea6f39ccf62331a251ae888b113180ba41
Denis Kryvchuk <dendenysyk@gmail.com>
2023-06-08 11:10:00 +0300

    p2p tests

--- p2p/main.py
+++ p2p/main.py
@@ -1,38 +1,44 @@
 import time
+import unittest
 
 
 class Peer:
     def __init__(self, name):
         self.name = name
-        self.message_queue = []
+        self.received_messages = []
 
     def send_message(self, recipient, message):
-        self.message_queue.append((recipient, message))
-
-    def process_messages(self):
-        while self.message_queue:
-            recipient, message = self.message_queue.pop(0)
-            print(f"{self.name} sends message to {recipient.name}: {message}")
-            time.sleep(0.5)  # Simulate network latency
-            recipient.receive_message(self, message)
+        recipient.receive_message(self.name, message)
 
     def receive_message(self, sender, message):
-        print(f"{self.name} receives message from {sender.name}: {message}")
+        self.received_messages.append((sender, message))
 
 
-# Usage
+class PeerToPeerTest(unittest.TestCase):
+    def test_peer_to_peer_communication(self):
+        alice = Peer("Alice")
+        bob = Peer("Bob")
+        charlie = Peer("Charlie")
+
+        alice.send_message(bob, "Hello Bob!")
+        bob.send_message(charlie, "Hi Charlie!")
+        charlie.send_message(alice, "Hey Alice!")
+
+        time.sleep(1)  # Wait for message propagation
+
+        # Assert messages received by peers
+        self.assertEqual(len(alice.received_messages), 1)
+        self.assertEqual(alice.received_messages[0][0], "Charlie")
+        self.assertEqual(alice.received_messages[0][1], "Hey Alice!")
+
+        self.assertEqual(len(bob.received_messages), 1)
+        self.assertEqual(bob.received_messages[0][0], "Alice")
+        self.assertEqual(bob.received_messages[0][1], "Hello Bob!")
+
+        self.assertEqual(len(charlie.received_messages), 1)
+        self.assertEqual(charlie.received_messages[0][0], "Bob")
+        self.assertEqual(charlie.received_messages[0][1], "Hi Charlie!")
+
+
 if __name__ == '__main__':
-    # Create peers
-    alice = Peer("Alice")
-    bob = Peer("Bob")
-    charlie = Peer("Charlie")
-
-    # Exchange messages
-    alice.send_message(bob, "Hello Bob!")
-    bob.send_message(charlie, "Hi Charlie!")
-    charlie.send_message(alice, "Hey Alice!")
-
-    # Process messages
-    alice.process_messages()
-    bob.process_messages()
-    charlie.process_messages()
+    unittest.main()
