2016051f9e1740714c3f53b2e57a41d2d7047609
Denis Kryvchuk <dendenysyk@gmail.com>
2023-06-08 11:13:24 +0300

    event bus test

--- event-bus/main.py
+++ event-bus/main.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class EventBus:
     def __init__(self):
         self.subscribers = {}
@@ -17,16 +20,35 @@ class EventBus:
                 callback(data)
 
 
-# Usage
+class EventBusTest(unittest.TestCase):
+    def test_event_bus(self):
+        bus = EventBus()
+
+        events = []
+
+        def event_callback_1(data):
+            events.append(f"Callback 1: {data}")
+
+        def event_callback_2(data):
+            events.append(f"Callback 2: {data}")
+
+        bus.subscribe("event_type", event_callback_1)
+        bus.subscribe("event_type", event_callback_2)
+
+        bus.publish("event_type", "Hello, World!")
+
+        self.assertEqual(len(events), 2)
+        self.assertIn("Callback 1: Hello, World!", events)
+        self.assertIn("Callback 2: Hello, World!", events)
+
+        bus.unsubscribe("event_type", event_callback_2)
+
+        bus.publish("event_type", "This event won't trigger the second callback.")
+
+        self.assertEqual(len(events), 3)
+        self.assertIn("Callback 1: This event won't trigger the second callback.", events)
+        self.assertNotIn("Callback 2:", events)
+
+
 if __name__ == '__main__':
-    def event_callback(data):
-        print("Event occurred:", data)
-
-    bus = EventBus()
-    bus.subscribe("event_type", event_callback)
-
-    bus.publish("event_type", "Hello, World!")
-
-    bus.unsubscribe("event_type", event_callback)
-
-    bus.publish("event_type", "This event won't trigger the callback.")
+    unittest.main()
